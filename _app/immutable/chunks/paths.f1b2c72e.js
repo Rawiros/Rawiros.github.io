fa223ebee8ffbb4d128fe5d684d6633be1375405
Rawiros <45668076+Rawiros@users.noreply.github.com>
2023-07-09 10:39:19 +0000

    Updates

---
+++ _app/immutable/chunks/paths.f1b2c72e.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_4dvrz2)==null?void 0:s.base)??"/rawiros.github.io";var a;const e=((a=globalThis.__sveltekit_4dvrz2)==null?void 0:a.assets)??t;export{e as a,t as b};
